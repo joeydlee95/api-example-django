4f8bb43025333caa0ca895440b8a6970ecc10fee
joeydlee95 <joeydonglee@gmail.com>
2018-01-18 21:10:14 -0800

    Update code structure to pep8 except for long line in view.py

--- drchrono/helper.py
+++ drchrono/helper.py
@@ -3,141 +3,137 @@ import datetime
 
 
 def get_authorization_header(request):
-  social = request.user.social_auth.get(provider='drchrono')
-  access_token = social.extra_data['access_token']
-  authorization = 'Bearer %s' % access_token
-  return { 'Authorization': authorization, }
+    """Returns a dict for Authorization token"""
+    social = request.user.social_auth.get(provider='drchrono')
+    access_token = social.extra_data['access_token']
+    authorization = 'Bearer %s' % access_token
+    return {'Authorization': authorization, }
 
 
 def make_api_get_request(request, url, params=None):
-  """ Returns the data if successful, or none"""
-  if not url:
-    return None
+    """Returns the data if successful, or none"""
+    if not url:
+        return None
 
-  headers = get_authorization_header(request=request)
-  response = requests.get(url=url, headers=headers, params=params)
-  if response.status_code == requests.codes.ok:
-    return response.json()
-  else:
-    return None
+    headers = get_authorization_header(request=request)
+    response = requests.get(url=url, headers=headers, params=params)
+    if response.status_code == requests.codes.ok:
+        return response.json()
+    else:
+        return None
 
 
 def make_api_patch_request(request, url, data, params=None):
-  """ Returns True if successful, False otherwise """
-  if not data or not url:
-    return False
+    """Returns True if successful, False otherwise"""
+    if not data or not url:
+        return False
 
-  headers = get_authorization_header(request=request)
-  response = requests.patch(url=url, headers=headers, params=params, data=data)
-  if response.status_code == requests.codes.no_content: # patch should return 204
-    return True
-  else:
-    return False
+    headers = get_authorization_header(request=request)
+    response = requests.patch(url=url, headers=headers,
+                              params=params, data=data)
+    if response.status_code == requests.codes.no_content:
+        return True
+    else:
+        return False
 
 
 def get_patient_id(request, first_name, last_name):
-  params = {
-    'first_name': first_name,
-    'last_name': last_name,
-  }
+    params = {'first_name': first_name,
+              'last_name': last_name, }
 
-  url = 'https://drchrono.com/api/patients_summary'
-  data = make_api_get_request(request=request, url=url, params=params)
-  
-  if data:
-    appointments = data['results']
-    if appointments[0]['id']:
-      return appointments[0]['id']
+    url = 'https://drchrono.com/api/patients_summary'
+    data = make_api_get_request(request=request, url=url, params=params)
 
-  return None
+    if data:
+        appointments = data['results']
+        if appointments[0]['id']:
+            return appointments[0]['id']
+
+    return None
 
 
 def get_appointment_id(request, patient_id):
-  date_today = datetime.date.today().isoformat()
-  params = {
-    'date': date_today,
-    'patient': patient_id,
-  }
+    date_today = datetime.date.today().isoformat()
+    params = {'date': date_today,
+              'patient': patient_id, }
 
-  url = 'https://drchrono.com/api/appointments'
-  data = make_api_get_request(request=request, url=url, params=params)
+    url = 'https://drchrono.com/api/appointments'
+    data = make_api_get_request(request=request, url=url, params=params)
 
-  if data:
-    appointments = data['results']
+    if data:
+        appointments = data['results']
 
-    for appointment in appointments:
-      if str(appointment['patient']) == patient_id:
-        return appointment['id']
-  return None
+        for appointment in appointments:
+            if str(appointment['patient']) == patient_id:
+                return appointment['id']
+    return None
 
 
 def get_patient_info(request, patient_id):
 
-  url = 'https://drchrono.com/api/patients/' + str(patient_id)
-  data = make_api_get_request(request=request, url=url)
+    url = 'https://drchrono.com/api/patients/' + str(patient_id)
+    data = make_api_get_request(request=request, url=url)
 
-  if data != None:
-    return data
+    if not data:
+        return data
 
-  return None
+    return None
 
 
 def update_arrived(request, appointment_id):
 
-  data = {
-    'status': 'Arrived',
-  }
+    data = {'status': 'Arrived', }
 
-  url = 'https://drchrono.com/api/appointments/' + appointment_id
-  response = make_api_patch_request(request=request, url=url, data=data)
-  if response:
-    return True
-  else:
-    return False
+    url = 'https://drchrono.com/api/appointments/' + appointment_id
+    response = make_api_patch_request(request=request, url=url, data=data)
+    if response:
+        return True
+    else:
+        return False
+
 
 def update_demographic(request, patient_id, form):
 
-  data = {
-    'email': form['email'],
-    'gender': form['gender'],
-  }
+    data = {'email': form['email'],
+            'gender': form['gender'], }
 
-  url = 'https://drchrono.com/api/patients/' + str(patient_id)
-  try:
-    response = make_api_patch_request(request=request, url=url, data=data)
-  except DoesNotExist:
-    return False
+    url = 'https://drchrono.com/api/patients/' + str(patient_id)
+    try:
+        response = make_api_patch_request(request=request, url=url, data=data)
+    except DoesNotExist:
+        return False
 
-  return response
+    return response
+
 
 def check_doctor_id(request, doctor_id):
-  url = 'https://drchrono.com/api/users/current'
-  data = make_api_get_request(request=request, url=url)
-  if data:
-    if str(data['doctor']) == doctor_id:
-      return True
-  return False
+    url = 'https://drchrono.com/api/users/current'
+    data = make_api_get_request(request=request, url=url)
+    if data:
+        if str(data['doctor']) == doctor_id:
+            return True
+    return False
+
 
 def today_appointment_list(request):
-  date_today = datetime.date.today().isoformat()
+    date_today = datetime.date.today().isoformat()
 
-  # TODO: must disable auto suggestions
-  params = {
-    'date': date_today,
-  }
+    # TODO: must disable auto suggestions
+    params = {'date': date_today, }
 
-  url = 'https://drchrono.com/api/appointments'
-  try:
-    data = make_api_get_request(request=request, url=url, params=params)
-  except DoesNotExist:
-    return None
-  return data['results']
+    url = 'https://drchrono.com/api/appointments'
+    try:
+        data = make_api_get_request(request=request, url=url, params=params)
+    except DoesNotExist:
+        return None
+    return data['results']
+
 
 def get_patient_summary(request, patient_id):
 
-  url = 'https://drchrono.com/api/patients_summary/' + str(patient_id)
-  try:
-    data = make_api_get_request(request=request, url=url)
-  except DoesNotExist:
-    return None
-  return data
+    url = 'https://drchrono.com/api/patients_summary/' + str(patient_id)
+    try:
+        data = make_api_get_request(request=request, url=url)
+    except DoesNotExist:
+        return None
+    return data
--- drchrono/models.py
+++ drchrono/models.py
@@ -2,64 +2,59 @@ from django.db import models
 from django.conf import settings
 
 import datetime
-#from localflavor.us.forms import USSocialSecurityNumberField
+# from localflavor.us.forms import USSocialSecurityNumberField
 
 MAX_NAME_LENGTH = settings.MAX_NAME_LENGTH
+MAX_ID_LENGTH = settings.MAX_ID_LENGTH
 
-# Create your models here.
-'''class Doctor(models.Model):
-  doctor_id = models.AutoField(primary_key=True)
-  last_name = models.CharField(max_length=MAX_NAME_LENGTH)
-  first_name = models.CharField(max_length=MAX_NAME_LENGTH)
-'''
 class Patient(models.Model):
-  patient_id = models.CharField(primary_key=True, max_length=MAX_NAME_LENGTH)
-  first_name = models.CharField(max_length=MAX_NAME_LENGTH)
-  last_name = models.CharField(max_length=MAX_NAME_LENGTH)
+    """A model for a patient"""
+    patient_id = models.CharField(primary_key=True, max_length=MAX_ID_LENGTH)
+    first_name = models.CharField(max_length=MAX_NAME_LENGTH)
+    last_name = models.CharField(max_length=MAX_NAME_LENGTH)
+    # ssn = USSocialSecurityNumberField()
 
-  def __unicode__(self):
-    return "%s %s" % (self.first_name, self.last_name)
-  #ssn = USSocialSecurityNumberField()
+    def __unicode__(self):
+        return "%s %s" % (self.first_name, self.last_name)
 
 
 class Appointment(models.Model):
-  appointment_id = models.CharField(max_length=MAX_NAME_LENGTH)
-  date_appointment = models.DateField()
-  scheduled_time =  models.DateTimeField()
-  exam_room = models.IntegerField()
-  duration = models.IntegerField()
-  doctor_id = models.CharField(max_length=MAX_NAME_LENGTH, blank=True, null=True)
-  patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    """A Model for an appointment."""
+    appointment_id = models.CharField(max_length=MAX_ID_LENGTH)
+    date_appointment = models.DateField()
+    scheduled_time = models.DateTimeField()
+    exam_room = models.IntegerField()
+    duration = models.IntegerField()
+    doctor_id = models.CharField(max_length=MAX_NAME_LENGTH,
+                                 blank=True, null=True)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
 
+    checkin_time = models.DateTimeField(blank=True, null=True)
+    wait_time = models.DurationField(blank=True, null=True)
+    status = models.CharField(max_length=MAX_NAME_LENGTH, null=True)
 
-  checkin_time = models.DateTimeField(blank=True, null=True)
-  wait_time = models.DurationField(blank=True, null=True)
-  status = models.CharField(max_length=MAX_NAME_LENGTH, null=True)
+    is_archived = models.BooleanField(default=False)
+    is_currently_seen = models.BooleanField(default=False)
 
-  is_archived = models.BooleanField(default=False)
-  is_currently_seen = models.BooleanField(default=False)
+    def full_name(self):
+        return str(self.patient)
 
-  def full_name(self):
-    return str(self.patient)
+    def __unicode__(self):
+        return "%s" % (self.appointment_id)
 
-  def __unicode__(self):
-    return "%s" % (self.appointment_id)
+    def get_readable_scheduled_time(self):
+        return self.scheduled_time.strftime("%Y-%m-%d %I:%M %p")
 
-  def get_readable_scheduled_time(self):
-    return self.scheduled_time.strftime("%Y-%m-%d %I:%M %p")
+    def get_readable_checkin_time(self):
+        if self.checkin_time:
+            checkin_time = self.checkin_time.strftime("%Y-%m-%d %I:%M %p")
+        else:
+            checkin_time = ""
+        return checkin_time
 
-  def get_readable_checkin_time(self):
-    if self.checkin_time:
-      checkin_time = self.checkin_time.strftime("%Y-%m-%d %I:%M %p")
-    else:
-      checkin_time = ""
-    return checkin_time
-
-  def get_wait_time(self):
-    if self.wait_time:
-      wait_time = self.wait_time
-    else:
-      wait_time = None
-    return wait_time
-
-
+    def get_wait_time(self):
+        if self.wait_time:
+            wait_time = self.wait_time
+        else:
+            wait_time = None
+        return wait_time
