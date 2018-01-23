c30afe8a85c79a78af0d0d3c8d7c023dfb99d5c8
joeydlee95 <joeydonglee@gmail.com>
2018-01-22 16:25:20 -0800

    Remove deprecated functions

--- drchrono/templatetags/drchrono_extras.py
+++
@@ -1,8 +0,0 @@
-from django import template    
-register = template.Library()    
-
-@register.filter('timestamp_to_time')
-def convert_timestamp_to_time(timestamp):
-    import datetime
-    # ISO 8601 specific
-    return datetime.datetime.strptime( timestamp, "%Y-%m-%dT%H:%M:%S" )
